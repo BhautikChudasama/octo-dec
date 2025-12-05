8194982719d4d84a263e2b850c6887b383a96d7e
Sid-NodeOps <sid@nodeops.xyz>
2025-12-05 18:05:29 +0530

    pricing update tier

--- backend/app/core/constants.py
+++ backend/app/core/constants.py
@@ -5,38 +5,38 @@ TIERS_FEATURE_LIMITS = {
                                 "projects": 1,
                                 "environments": 1,
                                 "build_minutes": 30,
-                                "vcpu_cu": 50,
-                                "vmemory_GB": 4,
+                                "vcpu_cu": 1,
+                                "vmemory_GB": 2,
                                 }
         },
     "Pro": {
             "credits" : 10000,
             "feature_limits" : {
-                                "projects": 1,
-                                "environments": 1,
-                                "build_minutes": 30,
-                                "vcpu_cu": 50,
-                                "vmemory_GB": 4,
+                                "projects": 5,
+                                "environments": 5,
+                                "build_minutes": 300,
+                                "vcpu_cu": 2,
+                                "vmemory_GB": 10,
                                 }
         },
     "Team": {
             "credits" : 50000,
             "feature_limits" : {
-                                "projects": 1,
-                                "environments": 1,
-                                "build_minutes": 30,
-                                "vcpu_cu": 50,
-                                "vmemory_GB": 4,
+                                "projects": 15,
+                                "environments": 15,
+                                "build_minutes": 1200,
+                                "vcpu_cu": 5,
+                                "vmemory_GB": 10,
                                 }
         },
     "Scale": {
             "credits" : 500000,
             "feature_limits" : {
-                                "projects": 1,
-                                "environments": 1,
-                                "build_minutes": 30,
+                                "projects": 1000,
+                                "environments": 1000,
+                                "build_minutes": 300000,
                                 "vcpu_cu": 50,
-                                "vmemory_GB": 4,
+                                "vmemory_GB": 1000,
                                 }
         },
     
